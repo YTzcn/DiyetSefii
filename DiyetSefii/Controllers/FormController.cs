bc9499e8cafa7a171659e2fa940765d73af389d0
Yahya Tezcan <yahya.tezcan@hotmail.com>
2023-05-30 01:57:07 +0300

    Proje dosyası ekle.

---
+++ DiyetSefii/Controllers/FormController.cs
@@ -0,0 +1,24 @@
+﻿using DiyetSefii.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiyetSefii.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Result(Diyet diyet)
+        {
+            //swhtch case kullan 
+            return View();
+        }
+    }
+}
---
+++ DiyetSefii/Models/Diyet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiyetSefii.Models
+{
+    public class Diyet
+    {
+        public int age { get; set; }
+        public int weight { get; set; }
+        public int gender { get; set; }
+        public int activity { get; set; }
+        public int diabetes { get; set; }
+        public int heartdisease { get; set; }
+        public int waterintake { get; set; }
+        public int hypertension { get; set; }
+        public int fastfoodintake { get; set; }
+        public int exercisehabit { get; set; }
+        public int cuisinepreference { get; set; }
+        public int weightgain { get; set; }
+        public int weightmaintenance { get; set; }
+        public int weightloss { get; set; }
+    
+    }
+}
